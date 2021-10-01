dbfa24f298da017c94686f01490d51bcf2a8f3ba
Rholdan <roldanhollow@gmail.com>
2021-09-30 20:00:06 -0500

    RRHH-API: Project base, h2 and libs

---
+++ src/main/java/com/leantech/rrhh/RRHHApplication.java
@@ -0,0 +1,11 @@
+package com.leantech.rrhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RRHHApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RRHHApplication.class, args);
+    }
+}
